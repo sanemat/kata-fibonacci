2ea1fef81d19e19f515b1af3fdefad2a22a53372
sanemat <o.gata.ken@gmail.com>
2010-09-02 23:50:44 +0900

    test refactaring

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -3,28 +3,23 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'fibonacci'
 require 'spec'
 describe "Fibonacci" do
+  let(:fibonacci){ Fibonacci.new }
   it "put 0" do
-    fibonacci = Fibonacci.new
     fibonacci.number(0).should == 0
   end
   it "put 1" do
-    fibonacci = Fibonacci.new
     fibonacci.number(1).should == 1
   end
   it "put 2" do
-    fibonacci = Fibonacci.new
     fibonacci.number(2).should == 1
   end
   it "put 3" do
-    fibonacci = Fibonacci.new
     fibonacci.number(3).should == 2
   end
   it "put 10" do
-    fibonacci = Fibonacci.new
     fibonacci.number(10).should == 55
   end
   it "put 100" do
-    fibonacci = Fibonacci.new
     fibonacci.number(100).should == 354224848179261915075
   end
 end
