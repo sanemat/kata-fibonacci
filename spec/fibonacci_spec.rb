3c5bbe9eb16d9fe3f747c2916ffc67d0d3ca0876
sanemat <o.gata.ken@gmail.com>
2010-09-02 22:45:51 +0900

    fix comment and delete initialize

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -3,8 +3,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'fibonacci'
 require 'spec'
 describe "Fibonacci" do
-  it "" do
-    lambda {Fibonacci.new}.should_not raise_error
+  it "put 0" do
+    Fibonacci.number(0).should == 0
   end
   it "put 1" do
     Fibonacci.number(0).should == 0
