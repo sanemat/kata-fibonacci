312b2189a4a6bc15cc8ba886caf4f800e80708b4
sanemat <o.gata.ken@gmail.com>
2010-09-12 00:50:08 +0900

    use memoizable module awesome!

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -2,32 +2,17 @@
 require 'active_support/memoizable'
 class Fibonacci
   extend ActiveSupport::Memoizable
-  def initialize
-    @stored = {}
-  end
   def number(input)
-    unless restore_fibonacci(input).nil?
-      return restore_fibonacci(input)
-    end
     if input == 0
-      store_number(input, 0)
+      0
     elsif input == 1
-      store_number(input, 1)
+      1
     else
-      store_number(input, twice_before_fibonacci_plus_before_fibonacci(input))
+      twice_before_fibonacci_plus_before_fibonacci(input)
     end
-    restore_fibonacci input
   end
   memoize :number
   def twice_before_fibonacci_plus_before_fibonacci(input)
     number(input - 2) + number(input - 1)
-  end
-
-  def store_number(input, fibonacci_number)
-    @stored[input] = fibonacci_number
-  end
-
-  def restore_fibonacci(input)
-    @stored[input] || nil
   end
 end
